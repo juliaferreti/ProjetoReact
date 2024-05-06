8c957863143788ec50f8e3d953ba3fbf11083463
Júlia Ferreti <113567445+juliaferreti@users.noreply.github.com>
2024-05-06 19:44:56 -0300

    minhasRotas.js

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import Home from "./Home";
 import Atividade01 from "./Atividade01";
+import Atividade02 from "./Atividade02";
 
 
 export default function MinhasRotas(){
@@ -10,6 +11,7 @@ export default function MinhasRotas(){
         <Routes>
             <Route path="/" element={<Home /> }/>
             <Route path="/Atividade01" element={<Atividade01 /> }/>
+            <Route path="/Atividade02" element={<Atividade02 /> }/>
         </Routes>
         </BrowserRouter>
     );
