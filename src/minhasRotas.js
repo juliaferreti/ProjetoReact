b518aca01b45a9cc0c1f01c99088349df809b3f1
Júlia Ferreti <113567445+juliaferreti@users.noreply.github.com>
2024-05-28 18:50:53 -0300

    minhasRotas.js

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Atividade01 from "./Atividade01";
 import Atividade02 from "./Atividade02";
 import Atividade03 from "./Atividade03";
+import Atividade04 from "./Atividade04";
 
 export default function MinhasRotas(){
     return (
@@ -13,6 +14,7 @@ export default function MinhasRotas(){
             <Route path="/Atividade01" element={<Atividade01 /> }/>
             <Route path="/Atividade02" element={<Atividade02 /> }/>
             <Route path="/Atividade03" element={<Atividade03 /> }/>
+            <Route path="/Atividade04" element={<Atividade04 /> }/>
         </Routes>
         </BrowserRouter>
     );
