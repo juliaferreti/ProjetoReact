b322fd8a5d34ceb53321123667d6b4c196e5aee2
Júlia Ferreti <113567445+juliaferreti@users.noreply.github.com>
2024-05-28 19:01:26 -0300

    AdicionandoInteratividade.js

--- src/Components/Atividade04/AdicionandoInteratividade.js
+++ src/Components/Atividade04/AdicionandoInteratividade.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { sculptureList } from './Data';
+import { useImmer } from 'use-immer';
 
 // Atividade 1
 function Toolbar({ onPlayMovie, onUploadImage }) {
